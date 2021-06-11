7143fd2a92dc6c31f433d3c9280982e19c4fb772
George Bouroutzoglou <geompokon@csd.auth.gr>
2021-06-11 08:05:30 +0300

    Change test size values
    much more for get and set
    bit less for remove because it acts up in arraylist sometimes (??)

--- src/main/java/gr/geompokon/bitarray/benchmark/state/TestStates.java
+++ src/main/java/gr/geompokon/bitarray/benchmark/state/TestStates.java
@@ -17,19 +17,19 @@ public class TestStates {
 
     @State(Scope.Benchmark)
     public static class GetTestSizeState {
-        @Param({"1000", "1000000", "5000000"}) // 10K, 1M, 5M
+        @Param({"5000000", "50000000", "250000000"}) // 5M, 50M, 250M
         public static int GET_TEST_SIZE;
     }
 
     @State(Scope.Benchmark)
     public static class SetTestSizeState {
-        @Param({"1000", "1000000", "5000000"}) // 10K, 1M, 5M
+        @Param({"5000000", "50000000", "250000000"}) // 5M, 50M, 250M
         public static int SET_TEST_SIZE;
     }
 
     @State(Scope.Benchmark)
     public static class RemoveTestSizeState {
-        @Param({"1000", "100000", "1500000"}) // 1K, 100K, 1M 500K
+        @Param({"1000", "100000", "1000000"}) // 1K, 100K, 1M 500K
         public static int REMOVE_TEST_SIZE;
     }
 }
