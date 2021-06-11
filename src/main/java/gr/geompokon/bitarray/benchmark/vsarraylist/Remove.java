612436bde876895668efced2c98588c7c2ca3824
George Bouroutzoglou <geompokon@csd.auth.gr>
2021-06-11 06:57:20 +0300

    Remove ok

--- src/main/java/gr/geompokon/bitarray/benchmark/vsarraylist/Remove.java
+++ src/main/java/gr/geompokon/bitarray/benchmark/vsarraylist/Remove.java
@@ -3,8 +3,8 @@ package gr.geompokon.bitarray.benchmark.vsarraylist;
 import gr.geompokon.bitarray.BitArray;
 import gr.geompokon.bitarray.benchmark.TestMethods;
 import gr.geompokon.bitarray.benchmark.state.ListState;
-import gr.geompokon.bitarray.benchmark.state.TestStates;
-import org.openjdk.jmh.annotations.Benchmark;
+import gr.geompokon.bitarray.benchmark.state.TestStates.RemoveTestSizeState;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
@@ -12,8 +12,13 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
-
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(5)
+@Warmup(iterations = 0, time = 1, timeUnit = TimeUnit.NANOSECONDS)
+@Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.NANOSECONDS)
 public class Remove {
 
     public static void main(String[] args) throws RunnerException {
@@ -27,10 +32,10 @@ public class Remove {
     public static class BitArrayState extends ListState<Boolean> {
         @Override
         public void setUp() {
-            obj = new BitArray(TestStates.RemoveTestSizeState.REMOVE_TEST_SIZE);
+            int elementsToAdd = RemoveTestSizeState.REMOVE_TEST_SIZE;
+            obj = new BitArray(elementsToAdd);
             // add the elements to remove in the benchmark
-            TestMethods.populateList(obj,
-                    TestStates.RemoveTestSizeState.REMOVE_TEST_SIZE,
+            TestMethods.populateList(obj, elementsToAdd,
                     ThreadLocalRandom.current()::nextBoolean);
         }
     }
@@ -38,25 +43,25 @@ public class Remove {
     public static class ArrayListState extends ListState<Boolean> {
         @Override
         public void setUp() {
-            obj = new ArrayList<>(TestStates.RemoveTestSizeState.REMOVE_TEST_SIZE);
+            int elementsToAdd = RemoveTestSizeState.REMOVE_TEST_SIZE;
+            obj = new ArrayList<>(elementsToAdd);
             // add the elements to remove in the benchmark
-            TestMethods.populateList(obj,
-                    TestStates.RemoveTestSizeState.REMOVE_TEST_SIZE,
+            TestMethods.populateList(obj, elementsToAdd,
                     ThreadLocalRandom.current()::nextBoolean);
         }
     }
 
     @Benchmark
-    public BitArrayState BitArrayRemove(BitArrayState bitArrayState) {
+    public BitArrayState BitArrayRemove(BitArrayState bitArrayState, RemoveTestSizeState testSizeState) {
         Random rand = ThreadLocalRandom.current();
-        TestMethods.removeRandomIndex(bitArrayState.obj, rand);
+        TestMethods.removeRandomIndex(bitArrayState.obj, rand, RemoveTestSizeState.REMOVE_TEST_SIZE);
         return bitArrayState;
     }
 
     @Benchmark
-    public ArrayListState ArrayListRemove(ArrayListState arrayListState) {
+    public ArrayListState ArrayListRemove(ArrayListState arrayListState, RemoveTestSizeState testSizeState) {
         Random rand = ThreadLocalRandom.current();
-        TestMethods.removeRandomIndex(arrayListState.obj, rand);
+        TestMethods.removeRandomIndex(arrayListState.obj, rand, RemoveTestSizeState.REMOVE_TEST_SIZE);
         return arrayListState;
     }
 }
